adfc152baca1e3416c73db5e0d33049070214e03
Prayag Bhatt <prayagbhatt2003@gmail.com>
2024-12-09 22:27:46 +0530

    feat: Day 9

--- Day9/main.go
+++ Day9/main.go
@@ -5,91 +5,86 @@ import (
 	"fmt"
 	"os"
 	"strconv"
+	"strings"
 )
 
-var diskMap string
-
-func input() {
+func input() string {
 	scanner := bufio.NewScanner(os.Stdin)
 	if scanner.Scan() {
-		diskMap = scanner.Text()
+		return scanner.Text()
 	}
+	return ""
 }
 
-func first() {
-	encodedStr := encode(diskMap)
-	compactStr := compact(encodedStr)
-	checksum := checkSum(compactStr)
-	fmt.Println("Original:", diskMap)
-	fmt.Println("Length:", len(diskMap))
-	fmt.Println("Checksum:", checksum)
-	fmt.Println("Encoded String:", encodedStr)
-	fmt.Println("Compacted String:", compactStr)
-}
+func makeFilesystem(disk string) []interface{} {
+	blocks := []interface{}{}
+	isFile := true
+	id := 0
 
-func second() {
-}
-
-func encode(diskMap string) string {
-	encodedStr := ""
-	fileId := 0
-
-	for i := 0; i < len(diskMap); i++ {
-		length, _ := strconv.Atoi(string(diskMap[i]))
-
-		for j := 0; j < length; j++ {
-			if i%2 == 0 {
-				encodedStr += strconv.Itoa(fileId)
-			} else {
-				encodedStr += "."
+	for _, char := range disk {
+		x, _ := strconv.Atoi(string(char))
+		if isFile {
+			for j := 0; j < x; j++ {
+				blocks = append(blocks, id)
 			}
-		}
-
-		if i%2 == 0 {
-			fileId++
-		}
-	}
-	return encodedStr
-}
-
-func compact(encodedStr string) string {
-	left := 0
-	right := len(encodedStr) - 1
-
-	for left <= right && right >= 0 {
-		if encodedStr[right] != '.' && encodedStr[left] == '.' {
-			encodedStr = encodedStr[:left] + string(encodedStr[right]) + encodedStr[left+1:right] + "." + encodedStr[right+1:]
-			left++
-			right--
-		} else if encodedStr[right] == '.' {
-			right--
+			id++
+			isFile = false
 		} else {
-			left++
+			for j := 0; j < x; j++ {
+				blocks = append(blocks, nil)
+			}
+			isFile = true
 		}
 	}
-
-	return encodedStr
+	return blocks
 }
 
-func checkSum(compactStr string) int {
-	checksum := 0
-
-	for i, char := range compactStr {
-		if char == '.' {
-			break
-		}
-		checksum += int(char-'0') * i
+func move(arr []interface{}) []interface{} {
+	firstFree := 0
+	for arr[firstFree] != nil {
+		firstFree++
 	}
 
-	return checksum
+	i := len(arr) - 1
+	for arr[i] == nil {
+		i--
+	}
+
+	for i > firstFree {
+		arr[firstFree] = arr[i]
+		arr[i] = nil
+		for arr[i] == nil {
+			i--
+		}
+		for arr[firstFree] != nil {
+			firstFree++
+		}
+	}
+	return arr
+}
+
+func checksum(arr []interface{}) int {
+	ans := 0
+	for i, x := range arr {
+		if x != nil {
+			ans += i * x.(int)
+		}
+	}
+	return ans
 }
 
 func solve() {
-	first()
-	second()
+	diskMap := input()
+	diskMap = strings.TrimSpace(diskMap)
+
+	filesystem := makeFilesystem(diskMap)
+	finalState := move(filesystem)
+	result := checksum(finalState)
+
+	fmt.Println("Length:", len(diskMap))
+	fmt.Println("Checksum:", result)
 }
 
 func main() {
-	input()
 	solve()
 }
